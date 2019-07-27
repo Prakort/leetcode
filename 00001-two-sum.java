03ff41621edf2b63c13a430032793e32ef1c8d5a
Prakort <leanprakort@gmail.com>
2019-07-26 22:46:46 -0400

    first commit

---
+++ 00001-two-sum.java
@@ -0,0 +1,52 @@
+class Solution {
+/*
+
+    We want to find the index of two element that add up to the target. 
+    We will add element to the next element in the list for every element
+    in order to check the sum.
+    If there are two sum , we will return the two index
+    #time complexity is n square
+    #space complexity is 1;
+
+*/
+    public int[] twoSum(int[] nums, int target) {
+        
+        for ( int i =0; i<nums.length ;i++)
+        {
+            for ( int j = i+1; j<nums.length ; j++)
+            {
+                if (nums[j]+nums[i]==target)
+                    return new int []{i,j}; // using new int []{} to instantiate a new array
+            }
+        
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+/*
+    This solution, we use an extra hashmap to store <element, index> 
+    of the complement Target - each element
+
+    If hashmap contains element return its value, index and nums[i]
+
+*/
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map <Integer,Integer> map = new HashMap<>();
+        
+        for ( int i =0; i<nums.length ;i++)
+        {
+            int complement = target - nums[i];
+            
+            if(!map.containsKey(complement))
+                map.put(nums[i],i);  
+            else
+               return new int []{map.get(complement),i};
+        }
+            throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+
+}

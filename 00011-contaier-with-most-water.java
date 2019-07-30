76fa4394a059828bfb5b5d3c59181c073d1ade29
Prakort <leanprakort@gmail.com>
2019-07-30 15:11:08 -0400

    day 3 two pointer solution added

--- 00011-contaier-with-most-water.java
+++ 00011-contaier-with-most-water.java
@@ -20,4 +20,37 @@ class Solution {
         }        
         return max;
         }
+
+
+    /*
+    Two pointer solution:
+
+    First we set start i point : 0 , end j pointer len -1
+
+    we calucate max area respect to the index pointers 
+
+    and compare to the previous max area value
+
+    We want to find the largest area so we move one of the point if its value is 
+
+    less than the other
+    */
+        public int maxArea2(int[] height) {
+    int max=0;
+    int n = height.length;
+    
+    int i = 0,j=n-1;
+    while(i<=j)
+    {
+        int width = j-i;
+        int hi = Math.min(height[i],height[j]);
+        
+        max=Math.max(max,width*hi);
+        
+        if(height[j]>height[i]) i++;
+        else j--;
+        
     }
+    return max;
+    }
+}

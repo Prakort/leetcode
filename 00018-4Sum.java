e4855656420bdfd18267be5a8a34ff7db0777d71
Prakort <leanprakort@gmail.com>
2019-08-01 19:04:58 -0400

    Day 5 4sum 2 pointer O(n): n*3

--- 00018-4Sum.java
+++ 00018-4Sum.java
@@ -51,4 +51,53 @@ class Solution {
             return list;
             
         }
+        /*
+
+Double two pointers from the end 
+O(n): n*3 + nlogn with sorting arrays
+
+*/
+    public List<List<Integer>> fourSum2(int[] nums, int target) {
+        HashSet<ArrayList<Integer>> set = new HashSet<>();
+        
+        Arrays.sort(nums);
+        
+        int n = nums.length;
+        // i < n-2 no need to go to the end since j , k , m get value to add to the sum 
+        for ( int i = 0 ; i < n -2; i++)
+        {   
+            for (int j = i+1; j< n-1; j++)
+            {
+                int k=j+1;
+                int h= n-1;
+                while(k<h)
+                {
+                        int a = nums[i];
+                        int b = nums[j];
+                        int c = nums[k];
+                        int d = nums[h];
+                        
+                        int sum = a+b+c+d;
+                        
+                        if ( sum==target)
+                        {
+                            set.add((new ArrayList(Arrays.asList(a,b,c,d))));
+                            k++;
+                            h--;
+                        }
+                        else if(sum > target)
+                        {
+                            h--;
+                        }
+                        else
+                        {
+                            k++;
+                        }
+                    }
+                }
+            }
+        // return set into arraylist <>
+        return new ArrayList<>(set);
+        
+    }
     }

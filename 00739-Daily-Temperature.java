e747821b0cc907c29d4c02318e4b9b0d51241c7e
Prakort <leanprakort@gmail.com>
2019-08-06 16:42:17 -0400

    Day 9 2 loop or stack

--- 00739-Daily-Temperature.java
+++ 00739-Daily-Temperature.java
@@ -24,4 +24,23 @@ class Solution {
         
         return arr;
     }
+    public int[] dailyTemperatures2(int[] arr) {
+        
+        int len = arr.length;
+        int result[] = new int[len];
+        Stack<Integer> stack = new Stack<>();
+        
+        for ( int i = 0 ; i <len ; i++)
+        {
+            while(!stack.isEmpty() && arr[i]>arr[stack.peek()])
+            {
+                int index = stack.pop();
+                result[index]= i - index;
+            }
+            stack.push(i);
+        }
+
+       
+        return result;
+    }
 }

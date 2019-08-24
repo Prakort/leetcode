1f4afc0ca478d187252556eb4f045c3388826e55
lean prakort <prakort@leans-iMac.local>
2019-08-24 00:13:35 -0400

    redo 560

---
+++ 00560-Subarray-Sum-Equals-K.java
@@ -0,0 +1,45 @@
+class Solution {
+    /*
+
+    Time: O(n*n)
+    Space: O(1)
+
+    */
+    public int subarraySum(int[] nums, int k) {
+  
+    int counter = 0;
+        
+    for ( int i =0 ; i < nums.length ; i++)
+    {
+        int sum = 0;
+        
+        for ( int j = i ; j< nums.length ; j++)
+        {
+    
+            sum +=nums[j];
+            if(sum==k)
+                counter++;
+        }
+    }   
+        return counter++;
+
+
+    public int second(int []arr,int k)
+    {
+        int count =0;
+        int sum = 0;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for ( int e : arr)
+        {
+            sum+=e;
+            if(map.containsKey(sum-k))
+                count+=map.get(sum-k);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+
+        }
+        return count;
+    }
+}
+    
